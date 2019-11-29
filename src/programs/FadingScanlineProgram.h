66002e17babbca29fb948e5f09a460da572b050b
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2019-11-29 11:38:56 +0100

    added a scanline program with fading trail

---
+++ src/programs/FadingScanlineProgram.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Globals.h"
+#include "Program.h"
+#include "../Utils.h"
+
+class FadingScanlineProgram : public Program
+{
+public:
+  FadingScanlineProgram() : Program(), hue(0) {}
+  int runIteration(uint8_t speed)
+  {
+    if (currentIteration % 32 == 0)
+      hue++;
+    int bpm = map(speed, 0, 255, 13, 50);
+
+    for (CRGB *ledStrip : ledStrips)
+    {
+      fadeToBlackBy(ledStrip, pixelsPerTube, 20);
+      int pos = beatsin16(bpm, 0, pixelsPerTube - 1);
+      ledStrip[pos] += CHSV(hue, 255, 255);
+    }
+    FastLED.show();
+    currentIteration++;
+
+    return 1;
+  }
+
+private:
+  uint8_t hue;
+};
