d7b082374ed6a5f7b69bd0c87dd4b4a78da74fb7
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2019-12-02 21:47:04 +0100

    added random strip sequence program

---
+++ src/programs/RandomStripSequenceFadingOutProgram.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "Program.h"
+#include "../Utils.h"
+
+class RandomStripSequenceFadingOutProgram : public Program
+{
+public:
+  RandomStripSequenceFadingOutProgram() : Program(),
+                                          previousStrip(-1),
+                                          hue(0),
+                                          maxStrips(3)
+  {
+  }
+
+  int runIteration(uint8_t speed)
+  {
+    int fadeSpeed = map(speed, 0, 255, 1, 4);
+    int lightUpSpeed = map(speed, 0, 255, 64, 16);
+
+    for (CRGB *ledStrip : ledStrips)
+    {
+      fadeToBlackBy(ledStrip, pixelsPerTube, fadeSpeed);
+    }
+
+    if (currentIteration % lightUpSpeed == 0)
+    {
+      int amountOfStripsToLight = getRandomNumber(1, maxStrips);
+      int stripToAvoid; // to avoid lighting the same strip(s) twice
+
+      for (int i = 0; i < amountOfStripsToLight; i++)
+      {
+        int newStrip = getRandomNumber(1, sizeof(ledStrips) / sizeof(ledStrips[0]), previousStrip);
+        fill_solid(ledStrips[newStrip - 1], pixelsPerTube, CHSV(hue, 255, 255));
+        stripToAvoid = newStrip;
+      }
+      previousStrip = stripToAvoid;
+      hue++;
+    }
+
+    FastLED.show();
+
+    currentIteration++;
+
+    return 1;
+  }
+
+private:
+  int previousStrip;
+  int hue;
+  int maxStrips;
+};
